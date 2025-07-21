802d36cf2b8743188b077d75f0e7f63ac6aa3dd9
Mayank Saharkar <mayanksaharkar@gmail.com>
2025-07-21 19:00:37 +0530

    feat : scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'; 
 import AppRoutes from './routes';
+import ScrollToTop from './components/common/ScrollToTop';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <ScrollToTop />
         <AppRoutes />
       </BrowserRouter>
     </>
